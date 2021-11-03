69b17e13a61568ae880d0a4f5510541384d518f4
HaleyOleynik-NOAA <haley.oleynik@noaa.gov>
2021-11-03 13:57:44 -0400

    Update RASAMS.cpp

--- inst/RASAMS.cpp
+++ inst/RASAMS.cpp
@@ -14,6 +14,22 @@ public:
   bool estimated;
 };
 
+// recruitment interface 
+struct recruitment_interface_base{
+  static std::map<uint32_t,recruitment_interface_base* > models;
+  static uint32_t id_g;
+  uint32_t id;
+};
+
+uint32_t recruitment_interface_base::id_g;
+std::map<uint32_t,recruitment_interface_base* > recruitment_interface_base::models;
+
+struct beverton_holt:public recruitment_interface_base{
+  parameter h;
+  parameter r0;
+  beverton_holt(){this->id=recruitment_interface_base::id_g++;}
+};
+
 RCPP_EXPOSED_CLASS(parameter)
 
     RCPP_MODULE(rasams) {
@@ -23,6 +39,10 @@ RCPP_EXPOSED_CLASS(parameter)
       .constructor<double>()
       .constructor<parameter>()
       .field("value", &parameter::value);
+    class_<beverton_holt >("beverton_holt")
+      .constructor()
+      .field("r0",&beverton_holt::r0)
+      .field("h",&beverton_holt::h);
     }
 
 
